495bad61314d12cd4e5376e7fe05e4bd60d12f5d
MoFayaz <fayazm0ist@gmail.com>
2024-07-03 20:13:27 +0530

    update

--- libs/S3.tsx
+++ libs/S3.tsx
@@ -1,13 +1,15 @@
+"use server"
+
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-    accessKeyId: String(process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID),
-    secretAccessKey: String(process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY),
-    region: String(process.env.NEXT_PUBLIC_AWS_REGION),
+    accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
+    secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
+    region: String(process.env.AWS_REGION),
 });
 
 const s3 = new AWS.S3();
 
-const s3Bucket = String(process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME);
+const s3Bucket = String(process.env.AWS_S3_BUCKET_NAME);
 
 export { s3, s3Bucket };
