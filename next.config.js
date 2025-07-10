efd8c087ad856ceb701a7f9647473d16f17e8b54
vedant dubey <dubeyvedant337@gmail.com>
2025-07-10 15:33:16 +0530

    chore: remove canvas, update config, and delete package-lock.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config, { isServer }) => {
-        // Add a rule to handle the canvas.node binary module
-        config.module.rules.push({ test: /\.node$/, use: "raw-loader" });
         config.experiments = {
             asyncWebAssembly: true,
             layers: true,
         };
-        // Exclude canvas from being processed by Next.js in the browser
-        if (!isServer) config.externals.push("canvas");
         return config;
     },
     images: {
