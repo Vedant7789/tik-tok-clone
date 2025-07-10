c5717f81d5b2f7e7698ace75e09b94417747b86c
vedant dubey <dubeyvedant337@gmail.com>
2025-07-11 00:56:46 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,25 @@ const nextConfig = {
             asyncWebAssembly: true,
             layers: true,
         };
+        const CopyWebpackPlugin = require('copy-webpack-plugin');
+        const path = require('path');
+
+        // Only copy for server build (prerendering)
+        if (isServer) {
+            config.plugins.push(
+                new CopyWebpackPlugin({
+                    patterns: [
+                        {
+                            from: path.resolve(
+                                __dirname,
+                                'node_modules/@sidan-lab/sidan-csl-rs-browser/sidan_csl_rs_bg.wasm'
+                            ),
+                            to: path.resolve(__dirname, '.next/server/chunks/'),
+                        },
+                    ],
+                })
+            );
+        }
         return config;
     },
     images: {
